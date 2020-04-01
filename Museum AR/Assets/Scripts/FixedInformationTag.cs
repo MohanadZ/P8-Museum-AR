64fb9379b3ed9f88c73b954f0e15fbc2fa42c07a
Leon Müller <44465413+metigel94@users.noreply.github.com>
2020-04-01 23:32:04 +0200

    UI Facing Camera + iPhone Model

---
+++ Museum AR/Assets/Scripts/FixedInformationTag.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FixedInformationTag : MonoBehaviour
+{
+    [SerializeField] Text nameLabel;
+
+    void Update()
+    {
+        FaceCamera();
+    }
+
+    private void FaceCamera()
+    {
+        Vector3 namePos = Camera.main.WorldToScreenPoint(this.transform.position);
+        nameLabel.transform.position = namePos;
+    }
+}
