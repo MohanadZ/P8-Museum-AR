1b55fbd9d3c7db912f4f8d8a6c8550ef3d3fa052
Leon Müller <44465413+metigel94@users.noreply.github.com>
2020-04-06 17:44:43 +0200

    Manual switching of guide views

---
+++ Museum AR/Assets/Scripts/SwitchGuideView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class SwitchGuideView : MonoBehaviour
+{
+    [SerializeField] ModelTarget m;
+    int i;
+
+    void Start()
+    {
+        i = 0;
+        m = gameObject.GetComponent<ModelTarget>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            
+            if(i == 0)
+            {
+                i = 1;
+                m.SetActiveGuideViewIndex(0);
+            } else
+            {
+                i = 0;
+                m.SetActiveGuideViewIndex(1);
+            }
+            
+        }
+    }
+}
