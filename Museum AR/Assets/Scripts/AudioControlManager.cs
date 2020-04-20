dbfddb83343d83c87e0556eeeb306fb7afabebcf
MohanadZ <mohanad.zn@hotmail.com>
2020-04-20 19:02:58 +0200

    NPC saying a random voice line
    Added functionality of having an NPC say a random line when pausing, playing or skipping an audio clip at an exhibit.

--- Museum AR/Assets/Scripts/AudioControlManager.cs
+++ Museum AR/Assets/Scripts/AudioControlManager.cs
@@ -8,15 +8,21 @@ public class AudioControlManager : MonoBehaviour
     [SerializeField] Button skipButton = null, playPauseButton = null;
     [SerializeField] Image npcTalkingIndicator = null, npcTalkingIndicatorIcon = null, playPauseIcon = null;
     [SerializeField] Sprite playSprite = null, npcNotTalkingIndicator = null;
+    [SerializeField] AudioClip[] pausingVoiceLines = null, uspausingVoiceLines = null, skippingVoiceLines = null;
 
     Sprite defaultPauseSprite, defaultTalkingIndicatorSprite;
     ExhibitAudioManager exhibitAudioManager;
     NPCManager npc;
+    AudioSource audioSource;
+
+    enum AudioAction { Pause, Unpause, Skip}
+    AudioAction audioAction;
 
     public Button SkipButton { get { return skipButton; } }
 
     void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
         exhibitAudioManager = FindObjectOfType<ExhibitAudioManager>();
         npc = FindObjectOfType<NPCManager>();
         defaultPauseSprite = playPauseIcon.GetComponent<Image>().sprite;
@@ -36,32 +42,32 @@ public class AudioControlManager : MonoBehaviour
         if (playPauseIcon.GetComponent<Image>().sprite == defaultPauseSprite)
         {
             playPauseIcon.GetComponent<Image>().sprite = playSprite;
-            exhibitAudioManager.GetAudioSource.Pause();
-            exhibitAudioManager.IsDisplayQuestions = false;
+            audioAction = AudioAction.Pause;
+            PlayRandomVoiceLine(pausingVoiceLines, audioAction);
         }
         else
         {
             playPauseIcon.GetComponent<Image>().sprite = defaultPauseSprite;
-            exhibitAudioManager.GetAudioSource.UnPause();
-            exhibitAudioManager.IsDisplayQuestions = true;
+            audioAction = AudioAction.Unpause;
+            PlayRandomVoiceLine(uspausingVoiceLines, audioAction);
         }
     }
 
     public void Skip()
     {
-        exhibitAudioManager.GetAudioSource.Stop();
-        exhibitAudioManager.IsDisplayQuestions = true;
+        playPauseIcon.GetComponent<Image>().sprite = defaultPauseSprite;
+        audioAction = AudioAction.Skip;
+        PlayRandomVoiceLine(skippingVoiceLines, audioAction);
 
-        if (exhibitAudioManager.AudioClipIndex < exhibitAudioManager.CurrentExhibitStory.Length)
-        {
-            playPauseIcon.GetComponent<Image>().sprite = defaultPauseSprite;
-            StopCoroutine(exhibitAudioManager.Coroutine);
-        }
+        //if (exhibitAudioManager.AudioClipIndex < exhibitAudioManager.CurrentExhibitStory.Length)
+        //{
+        //    //StopCoroutine(exhibitAudioManager.Coroutine);
+        //}
     }
 
     private void TalkingIcon()
     {
-        if (exhibitAudioManager.GetAudioSource.isPlaying)
+        if (exhibitAudioManager.GetAudioSource.isPlaying || audioSource.isPlaying)
         {
             npcTalkingIndicatorIcon.sprite = defaultTalkingIndicatorSprite;
             npcTalkingIndicator.gameObject.SetActive(true);
@@ -82,5 +88,71 @@ public class AudioControlManager : MonoBehaviour
         playPauseButton.gameObject.SetActive(false);
         skipButton.gameObject.SetActive(false);
         //npcTalkingIndicator.gameObject.SetActive(false);
+    }
+
+    private void PlayRandomVoiceLine(AudioClip[] randomAudioClip, AudioAction action)
+    {
+        int randomIndex = Random.Range(0, randomAudioClip.Length);
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(randomAudioClip[randomIndex]);
+        }
+
+        switch (action)
+        {
+            case AudioAction.Pause:
+                StartCoroutine(PauseExhibitAudioThenWait());
+                break;
+            case AudioAction.Unpause:
+                StartCoroutine(WaitThenUnpauseExhibitAudio());
+                break;
+            case AudioAction.Skip:
+                StartCoroutine(WaitThenSkipExhibitAudio());
+                break;
+        }
+    }
+
+    IEnumerator PauseExhibitAudioThenWait()
+    {
+        exhibitAudioManager.GetAudioSource.Pause();
+        exhibitAudioManager.IsDisplayQuestions = false;
+        DisableControlInteraction();
+
+        yield return new WaitUntil(() => !audioSource.isPlaying);
+        AllowControlInteraction();
+    }
+
+    IEnumerator WaitThenUnpauseExhibitAudio()
+    {
+        DisableControlInteraction();
+        yield return new WaitUntil(() => !audioSource.isPlaying);
+
+        exhibitAudioManager.GetAudioSource.UnPause();
+        exhibitAudioManager.IsDisplayQuestions = true;
+        AllowControlInteraction();
+    }
+
+    IEnumerator WaitThenSkipExhibitAudio()
+    {
+        exhibitAudioManager.GetAudioSource.Stop();
+        exhibitAudioManager.IsDisplayQuestions = false;
+        DisableControlInteraction();
+        yield return new WaitUntil(() => !audioSource.isPlaying);
+
+        exhibitAudioManager.IsDisplayQuestions = true;
+        AllowControlInteraction();
+    }
+
+    private void DisableControlInteraction()
+    {
+        playPauseButton.interactable = false;
+        skipButton.interactable = false;
+    }
+
+    private void AllowControlInteraction()
+    {
+        playPauseButton.interactable = true;
+        skipButton.interactable = true;
     }
 }
