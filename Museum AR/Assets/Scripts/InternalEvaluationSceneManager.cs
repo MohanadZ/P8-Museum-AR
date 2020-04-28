add13060a9d9908d47158d863aa692c1bcdf8e7b
Leon Müller <44465413+metigel94@users.noreply.github.com>
2020-04-28 14:24:37 +0200

    Created one scene for each ModelTarget

---
+++ Museum AR/Assets/Scripts/InternalEvaluationSceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InternalEvaluationSceneManager : MonoBehaviour
+{
+    public void SwitchToBox()
+    {
+        SceneManager.LoadScene("Box ModelTarget");
+    }
+
+    public void SwitchToSword()
+    {
+        SceneManager.LoadScene("Sword ModelTarget");
+    }
+
+    public void SwitchToTub()
+    {
+        SceneManager.LoadScene("Tub ModelTarget");
+    }
+
+}
---
+++ Museum AR/Assets/Scripts/InternalEvaluationUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InternalEvaluationUI : MonoBehaviour
+{
+    [SerializeField] Text detected;
+    Renderer objectRenderer;
+    void Start()
+    {
+        objectRenderer = GetComponent<MeshRenderer>();
+    }
+
+
+    void Update()
+    {
+        if (objectRenderer.enabled)
+        {
+            detected.enabled = true;
+        }
+        else
+        {
+            detected.enabled = false;
+        }
+    }
+}
